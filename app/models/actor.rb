1ce35d3b8276d903df05e454a01c5aeecb1834fb
nmhummel <natalie.hummel@gmail.com>
2020-11-24 14:34:05 -0600

    commit tree 21858f3f1176135af063cc850b0abb5de92d3a8a

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,33 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        #returns the first and last name of an actor.
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        #that lists all of the characters that actor has alongside the show that the character is in. 
+        self.characters.collect do |char|
+            "#{char.name} - #{char.show.name}"
+        end
+
+    end
+
+    # So, for instance, if we had an actor, Peter Dinklage, a character, Tyrion Lannister, 
+    # and a show, Game of Thrones:
+
+    # peter = Actor.new(:first_name => "Peter", :last_name => "Dinklage")
+    # tyrion = Character.new(:name => "Tyrion Lannister")
+    # tyrion.actor = peter
+    # thrones = Show.new(:name => "Game of Thrones")
+    # tyrion.show = thrones
+    # tyrion.save 
+
+    # And then, when we run peter.list_roles, we would get an Array containing a string with both 
+    # the character and the show:
+    # ['Tyrion Lannister - Game of Thrones']
 end
+
+# An actor has many characters and has many shows through characters.
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
+  end
+  # returns an Array of the full names of each actor associated with the a show. 
+
 end
+
+# A show has many characters and has many actors through characters.
